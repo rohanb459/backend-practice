d8a6343e2abeeaedce000a1d30a6e43e77ce6016
Rohan Bhatia <rohanb459@gmail.com>
2022-06-10 23:42:45 +0530

    c

--- methods.js
+++ methods.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());//middleware func-> post, front->json
-app.listen(3000);
+app.listen(3000); 
 
 let users={};
 
@@ -12,8 +12,33 @@ app.get('/users', (req,res)=>{
 
 app.post('/users',(req,res)=>{
     console.log(req.body);
+    users=req.body;
     res.json({
         message: "data received successfully",
         user: req.body
-    });
+    })
 }); 
+
+
+// update -> patch
+app.patch('/users', (req,res)=>{
+    console.log('req body ->', req.body);
+    // update data in users obj
+    let dataToBeUpdated=req.body;
+    for(key in dataToBeUpdated)
+    {
+        users[key]=dataToBeUpdated[key];
+    }
+    res.json({
+        message: "data updated successfully"
+    });
+})
+
+// delete
+
+app.delete('/users', (req,res)=>{
+    users={};
+    res.json({
+        message: "data has been deleted"
+    });
+});
