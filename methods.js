a8692505b1dd40f58c165939fb012f2fdcac6f80
Rohan Bhatia <rohanb459@gmail.com>
2022-06-10 22:59:50 +0530

    c

---
+++ methods.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());//middleware func-> post, front->json
+app.listen(3000);
+
+let users={};
+
+app.get('/users', (req,res)=>{
+    res.send(users);
+})
+
+app.post('/users',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        message: "data received successfully",
+        user: req.body
+    });
+}); 
