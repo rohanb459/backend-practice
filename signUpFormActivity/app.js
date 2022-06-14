fadcd54f15e9171e00ece408018ec90e20e2adcc
Rohan Bhatia <rohanb459@gmail.com>
2022-06-15 00:34:50 +0530

    hooks implemented

--- signUpFormActivity/app.js
+++ signUpFormActivity/app.js
@@ -162,6 +162,14 @@ const userSchema = mongoose.Schema({
 
 });
 
+userSchema.pre("save", function(){
+    console.log('before saving in db');
+});
+
+userSchema.post("save", function(doc){
+    console.log('after saving in db', doc);
+});
+
 const userModel=mongoose.model('userModel', userSchema);
 
 // (async function createUser()
